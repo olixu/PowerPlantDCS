fdd32b3fd5d7ce3c666dd981754e57b040e9114c
olixu <273601727@qq.com>
2019-03-06 09:08:13 +0800

    导入数据改变为读取有效率数据的CSV文件，但是效率还未改动

--- CallhuodianWin.py
+++ CallhuodianWin.py
@@ -36,9 +36,7 @@ class InputDate_Thread(QThread):
 
     def run(self):
         starttime = datetime.datetime.now()
-        plant_data = pd.read_excel(self.filename[0])
-        plant_data.drop([0], inplace=True)
-        plant_data.drop([1], inplace=True)
+        plant_data = pd.read_csv(self.filename[0])
         endtime = datetime.datetime.now()
         time_cost = endtime-starttime
         self.sinOut.emit(str(time_cost), plant_data)
@@ -161,7 +159,7 @@ class MainWindow(QMainWindow):
         # 定义一些变量
 
         self.df_rows = 8
-        self.df_cols = 11
+        self.df_cols = 13
         self.plant_data = None
         # 初始化
         self.ui = Ui_Form()
@@ -174,7 +172,7 @@ class MainWindow(QMainWindow):
     # 数据预处理标签下的槽函数：导入数据->OpenFile()
     def OpenFile(self):
         self.filename = QFileDialog.getOpenFileName(
-            self, 'open file', '/media/boss/大学资料/大学4年学习资料/大四下/毕业设计所有资料/嘉兴数据/完整版/20160612-0624')
+            self, 'open file', '/home/oliverxu/桌面/github_projects/biyesheji/锅炉效率计算/包含效率数据/')
         self.ui.DataMiningLabel.setText("正在导入，文件较大，请等待")
         self.logger.info("正在导入，文件较大，请等待")
         self.DataMiningthread.get_filename(self.filename)
@@ -224,48 +222,26 @@ class MainWindow(QMainWindow):
 
     # 数据预处理标签下的槽函数：数据预处理->Calculate()
     def Calculate(self):
-        # plant_data.drop([1], inplace=True)
-        raw_给煤量 = ['给煤机A给煤量', '给煤机B给煤量', '给煤机C给煤量',
-                   '给煤机D给煤量', '给煤机E给煤量', '给煤机F给煤量']
-        raw_主蒸汽压力 = ['主蒸汽压力']
-        raw_炉膛出口烟温度 = ['炉膛烟温探针B']
-        raw_主蒸汽温度 = ['主蒸汽温度1']
-        raw_机组负荷 = ['负荷']
-        raw_给水温度 = ['省煤器进口给水温度']
-        raw_给水流量 = ['给水流量']
-        raw_一次风量 = ['总一次风量']
-        # raw_一次风温 = ['']
-        raw_二次风量 = ['A侧二次风量', 'B侧二次风量']
-        raw_二次风温 = ['二次风温度']
-        raw_含氧量 = ['省煤器A侧出口烟气氧量分析1', '省煤器 A侧出口烟气氧量分析2', '省煤器A侧出口烟气氧量分析3',
-                   '省煤器B侧出口烟气氧量分析1', '省煤器B侧出口烟气氧量分析2', '省煤器B侧出口烟气氧量分析3']
-        # raw_含氧量 = ['省煤器A侧出口烟气氧量分析3']
-        给煤量 = self.plant_data[raw_给煤量].sum(1)
-        主蒸汽压力 = self.plant_data[raw_主蒸汽压力]
-        炉膛出口烟温度 = self.plant_data[raw_炉膛出口烟温度]
-        主蒸汽温度 = self.plant_data[raw_主蒸汽温度]
-        机组负荷 = self.plant_data[raw_机组负荷]
-        给水温度 = self.plant_data[raw_给水温度]
-        给水流量 = self.plant_data[raw_给水流量]
-        一次风量 = self.plant_data[raw_一次风量]
-        二次风量 = self.plant_data[raw_二次风量].mean(1)
-        二次风温 = self.plant_data[raw_二次风温]
-        含氧量 = self.plant_data[raw_含氧量].mean(1)
-        fields = [给煤量, 主蒸汽压力, 炉膛出口烟温度, 主蒸汽温度, 机组负荷,
-                  给水温度, 给水流量, 一次风量, 二次风量, 二次风温, 含氧量]
-        outcome = pd.concat(fields, axis=1)
-        outcome.columns = ['给煤量', '主蒸汽压力', '炉膛出口烟温度', '主蒸汽温度',
-                           '机组负荷', '给水温度', '给水流量', '一次风量', '二次风量', '二次风温', '含氧量']
-        # outcome['机组负荷'].describe()
-        # outcome.dtypes
-        outcome = outcome.apply(pd.to_numeric)
-        self.data_pre_handle = outcome
-        ruanceliang_data = outcome.describe()
+        给煤量 = self.plant_data['给煤量']
+        排烟温度 = self.plant_data['排烟温度']
+        主蒸汽压力 = self.plant_data['主蒸汽压力']
+        炉膛出口烟温度 = self.plant_data['炉膛出口烟温度']
+        主蒸汽温度 = self.plant_data['主蒸汽温度']
+        机组负荷 = self.plant_data['机组负荷']
+        给水温度 = self.plant_data['给水温度']
+        给水流量 = self.plant_data['给水流量']
+        一次风量 = self.plant_data['一次风量']
+        二次风量 = self.plant_data['二次风量']
+        二次风温 = self.plant_data['二次风温']
+        含氧量 = self.plant_data['含氧量']
+        效率 = self.plant_data['效率']
+        self.data_pre_handle = self.plant_data
+        ruanceliang_data = self.plant_data.describe()
 
         self.ui.DataMiningTableWidget.setRowCount(self.df_rows)
         self.ui.DataMiningTableWidget.setColumnCount(self.df_cols)
         self.ui.DataMiningTableWidget.setHorizontalHeaderLabels(
-            ['给煤量', '主蒸汽压力', '炉膛出口烟温度', '主蒸汽温度', '机组负荷', '给水温度', '给水流量', '一次风量', '二次风量', '二次风温', '含氧量'])
+            ['给煤量', '排烟温度', '主蒸汽压力', '炉膛出口烟温度', '主蒸汽温度', '机组负荷', '给水温度', '给水流量', '一次风量', '二次风量', '二次风温', '含氧量', '效率'])
         self.ui.DataMiningTableWidget.setVerticalHeaderLabels(
             ['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
         for i in range(self.df_rows):
